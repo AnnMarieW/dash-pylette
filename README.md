2b2fb7c958cf146ba0dbe9ebb6074cc551b8dd11
AnnMarueW <amward@fastmail.com>
2024-05-05 16:30:38 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,62 @@
+
+# Dash Pylette
+
+### Introduction
+
+Welcome to Dash Pylette - a  Dash app showcasing the power of the Pylette library. This app serves as a tool to extract
+color palettes from images. Whether you're a designer, artist, or developer, Dash Pylette provides an easy
+way to generate color schemes for your projects.
+
+### Features
+
+- **User-Friendly Interface**: Easily select an image from the dropdown menu or upload your own to generate a color palette.
+- **Dynamic Color Picker**: Explore the extracted colors and choose the perfect frame color for the image.
+- **Customization Options**: Configure Pylette to extract colors using different algorithms and sorting methods.
+- **Integration with Pylette**: Utilizes the powerful features of the Pylette library for efficient color extraction.
+- **Responsive Design**: Designed with the Dash Mantine Components Library.
+
+###  See it live at:  
+
+### Getting Started
+
+To get started with Dash Pylette, simply install the Pylette library:
+
+
+Then, clone this repository and run the app:
+
+```bash
+git clone https://github.com/your-username/DashPylette.git
+cd DashPylette
+pip install -r requirements.txt
+python app.py
+```
+
+### Usage
+
+1. **Select Image**: Choose an image from the dropdown menu or upload your own.  
+
+Configure Pylette
+
+2. **Sort Colors By** - will sort the color palette by the frequency or the luminance (perceived brightness).
+3. **Color Quantization Mode** - to sepecify the color quantization method - either using K-Means (default) or Median-Cut algorithms.
+4. **Palette Color Count** - set the number of colors in the palette
+5. **Resize** - resize the image to a more manageable size before beginning color extraction. This significantly speeds up the extraction, but reduces the faithfulness of the color palette.
+2. **Generate Palette**: Let Pylette work its magic and extract a color palette from the selected image.
+
+Use the generated Color Palette
+5. **Explore**: Explore the extracted colors and pick your favorites using the color picker.
+4. **Set Frame Color**: Set the selected color as the frame color for the image to see it in action.
+5. **Experiment**: Try out different images, algorithms, and sorting methods to discover unique color combinations.
+
+
+### Find more information in the [Pylette GitHub](https://github.com/qTipTip/Pylette) 
+Don't forget to give both this library and Pylette a :star: !
+
+
+### License
+
+Dash Pylette is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+---
+
+#### **Happy Palette Crafting!** 🎨✨
---
+++ app.py
@@ -0,0 +1,274 @@
+import dash_mantine_components as dmc
+from dash_iconify import DashIconify
+from dash import Dash, _dash_renderer, html, dcc, callback, Input, Output, State, ctx
+from Pylette import extract_colors
+import tempfile
+import base64
+
+_dash_renderer._set_react_version("18.2.0")
+
+icons = {
+    "github": "ion:logo-github",
+    "tools": "bi:tools",
+    "palette": "bi:palette",
+}
+
+
+select_image = dmc.Select(
+    id="select-image",
+    label="Select sample image",
+    placeholder="Select one",
+    data=[
+        {"value": "assets/flowers.jpg", "label": "Flowers"},
+        {"value": "assets/fall.jpg", "label": "Fall in New England"},
+        {"value": "assets/duck.jpg", "label": "Duck"},
+        {"value": "assets/space_needle.jpg", "label": "Space Needle"},
+        {"value": "assets/mushrooms.jpg", "label": "Mushrooms"},
+        {"value": "assets/flowers_arrangement.jpg", "label": "Flower Arrangement"},
+    ],
+    w="100%",
+    mb=10,
+    value="assets/flowers.jpg",
+)
+
+upload = dcc.Upload(
+    dmc.NavLink(label="Drag and Drop or Upload an Image File", className="upload"),
+    multiple=False,
+    id="upload-image",
+)
+
+
+sort_by_data = [["frequency", "Frequency"], ["luminance", "Luminance"]]
+sort_by = dmc.RadioGroup(
+    children=dmc.Group([dmc.Radio(l, value=k) for k, l in sort_by_data], my=10),
+    id="radio-group-sort-by",
+    value="frequency",
+    label="Sort colors by",
+    size="sm",
+    mb=30,
+)
+
+
+mode_data = [["KM", "K-Means"], ["MC", "Median Cut"]]
+mode = dmc.RadioGroup(
+    children=dmc.Group([dmc.Radio(l, value=k) for k, l in mode_data], my=10),
+    id="radio-group-mode",
+    value="MC",
+    label="Color quantization mode",
+    size="sm",
+    mb=30,
+)
+
+
+resize = dmc.Checkbox(
+    id="checkbox-resize", label="Resize image for faster results", checked=False, mb=30
+)
+
+
+pallette_color_count = dmc.Slider(
+    value=6, min=1, step=1, max=12, w="100%", my=20, id="color-count"
+)
+
+
+set_frame_color = dmc.Checkbox(
+    id="checkbox-set-background", label="Set image frame color", checked=True, mb=10
+)
+
+
+color_picker = dmc.ColorPicker(
+    id="color-picker", swatchesPerRow=6, withPicker=False, mb=24
+)
+
+
+def create_link(icon, href):
+    return dmc.Anchor(
+        dmc.ActionIcon(
+            DashIconify(icon=icon, width=25), variant="transparent", size="lg"
+        ),
+        href=href,
+        target="_blank",
+        visibleFrom="xs",
+    )
+
+
+header = dmc.Group(
+    [
+        dmc.Burger(id="burger-button", opened=False),
+        dmc.Text(["Pylette: Extract a color palette from an image"], size="xl"),
+        create_link(icons["github"], "https://github.com/qTipTip/Pylette"),
+    ],
+    justify="flex-start",
+)
+
+
+def make_divider(text, icon):
+    return dmc.Divider(
+        label=[
+            DashIconify(icon=icon, height=23),
+            dmc.Text(text, ml=5, size="sm"),
+        ],
+        labelPosition="left",
+        mt=60,
+        mb=10,
+    )
+
+
+navbar = html.Div(
+    [
+        select_image,
+        dmc.Text("Or upload a file", size="sm"),
+        upload,
+        make_divider("Configure Pylette", icons["tools"]),
+        sort_by,
+        mode,
+        resize,
+        dmc.Text("Palette color count", size="sm"),
+        pallette_color_count,
+        make_divider("Results", icons["palette"]),
+        set_frame_color,
+        color_picker,
+        dmc.ScrollArea(html.Div(id="copy"), w=350),
+    ]
+)
+
+page_content = dcc.Loading(
+    [
+        dmc.Center(
+            html.Div(
+                html.Img(
+                    id="image", style={"height": 650, "width": "auto", "padding": 50}
+                ),
+                id="image-card",
+                style={"height": 650},
+            )
+        ),
+        dmc.Group(id="palette", gap=0, justify="center", mt=10),
+    ],
+    overlay_style={"visibility": "visible", "opacity": 0.5, "backgroundColor": "white"},
+)
+
+
+app = Dash(__name__)
+
+app_shell = dmc.AppShell(
+    [
+        dmc.AppShellHeader(header, px=25),
+        dmc.AppShellNavbar(navbar, p=24),
+        dmc.AppShellMain(page_content),
+    ],
+    header={"height": 70},
+    padding="xl",
+    navbar={
+        "width": 375,
+        "breakpoint": "md",
+        "collapsed": {"mobile": True},
+    },
+    id="app-shell",
+)
+
+app.layout = dmc.MantineProvider([app_shell])
+
+
+def save_base64_image(base64_string):
+    """
+    Save uploaded image to a temporary file
+    :param base64_string: image
+    :return: temporary path name
+    """
+    # Decode the base64 string into bytes
+    image_data = base64.b64decode(base64_string.split(",")[1])
+    # Create a temporary file
+    with tempfile.NamedTemporaryFile(suffix=".jpg", delete=False) as temp_file:
+        # Write the image data to the temporary file
+        temp_file.write(image_data)
+    return temp_file.name
+
+
+def make_palette(swatches):
+    palette = []
+    for color in swatches:
+        palette.append(
+            dmc.Tooltip(
+                dmc.Text(
+                    style={
+                        "backgroundColor": color,
+                        "borderLeft": "1px rgb(50, 50, 50) solid",
+                        "height": "24px",
+                        "width": "60px",
+                    }
+                ),
+                label=color,
+                withArrow=True,
+                position="bottom",
+            )
+        )
+    return palette
+
+
+@callback(
+    Output("image", "src"),
+    Output("color-picker", "swatches"),
+    Output("palette", "children"),
+    Output("color-picker", "value"),
+    Output("copy", "children"),
+    Input("select-image", "value"),
+    Input("upload-image", "contents"),
+    Input("color-count", "value"),
+    Input("radio-group-sort-by", "value"),
+    Input("radio-group-mode", "value"),
+    Input("checkbox-resize", "checked"),
+)
+def update_image(image_path, upload, color_count, sort_value, mode_value, resize_value):
+    """
+    Notes:
+        `extract_colors` accepts an image file path only.  If a file is uploaded, it's saved in a temporary file
+        the html.Img `src` prop is either the uploaded base64 file, or a path to a file in the assets folder.
+    """
+    src = image_path
+
+    if ctx.triggered_id == "upload-image":
+        temp_file_path = save_base64_image(upload)
+        image_path = temp_file_path
+        src = upload
+
+    palette = extract_colors(
+        image=image_path,
+        palette_size=color_count,
+        mode=mode_value,
+        sort_mode=sort_value,
+        resize=resize_value,
+    )
+
+    swatches = [f"rgb{color.rgb}" for color in palette]
+    dominant_color = swatches[0]
+
+    palette_swatch = make_palette(swatches)
+    copy_swatches = dmc.CodeHighlight(
+        code=str(swatches), language="python", copyLabel="copy color palette"
+    )
+
+    return src, swatches, palette_swatch, dominant_color, copy_swatches
+
+
+@callback(
+    Output("image-card", "style"),
+    Input("checkbox-set-background", "checked"),
+    Input("color-picker", "value"),
+)
+def update_frame_color(checked, color):
+    style = {"backgroundColor": color}
+    return style if checked else None
+
+
+@callback(
+    Output("app-shell", "navbar"),
+    Input("burger-button", "opened"),
+    State("app-shell", "navbar"),
+)
+def navbar_is_open(opened, navbar):
+    navbar["collapsed"] = {"mobile": not opened}
+    return navbar
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
